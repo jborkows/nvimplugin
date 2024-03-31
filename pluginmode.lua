6841c102f6044271f0209dacc7e814e56a15b2f6
jborkows <jacek.borkowskiit@gmail.com>
2024-03-31 03:17:51 +0200

    Enables fast development of plugins by reloading saved file.

---
+++ pluginmode.lua
@@ -0,0 +1,19 @@
+local logger = require("plenary.log"):new()
+logger.level = "debug"
+local pluginModeGroup = vim.api.nvim_create_augroup("plugin-mode-jb", { clear = true })
+
+local M = {}
+vim.api.nvim_create_user_command("PluginMode", function()
+	logger.debug(vim.api.nvim_buf_get_name(0))
+	vim.api.nvim_create_autocmd("BufWritePost", {
+		group = pluginModeGroup,
+		buffer = 0,
+		callback = function()
+			logger.debug("Save completed")
+			vim.cmd("source %")
+			logger.debug("Reloaded ->" .. vim.api.nvim_buf_get_name(0))
+		end
+	})
+end, {})
+
+return M
